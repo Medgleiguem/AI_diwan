ca0263ecebb9c7dfa17e7c4dfba8f6c2c3217eaa
med <rantuci44@gmial.com>
2026-02-22 18:48:58 +0000

    add speed insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 
 const qc = new QueryClient({
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   }}>
         <App />
         <Analytics />
+        <SpeedInsights />
         <Toaster
           position="top-center"
           toastOptions={{

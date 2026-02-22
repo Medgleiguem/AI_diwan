5150e220f2c823f1a3bfadfafbfafc8b17e80c27
med <rantuci44@gmial.com>
2026-02-22 18:31:05 +0000

    add analytics Analyzer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 
 const qc = new QueryClient({
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     v7_relativeSplatPath: true,
   }}>
         <App />
+        <Analytics />
         <Toaster
           position="top-center"
           toastOptions={{
